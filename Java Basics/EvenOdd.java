0060d1f1fe8ef2a8fc1440bde51013d7c237f4bd
PradiptaKumar <pradipta18.parida@gmail.com>
2023-09-19 11:47:37 +0530

    aqweeeeee

---
+++ Java Basics/EvenOdd.java
@@ -0,0 +1,11 @@
+// The numbers we will be getting as input from the user.
+// so, we need to use scanner class
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    System.out.println("Smallest element of array: "+ min);
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt(); // To take input as integer. nextline() or next() will take a string as input
+    
+}
